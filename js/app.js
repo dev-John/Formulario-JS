094b30664f36383a643be6afacc67a7059fbba14
Jonatas Oliveira <jonatasoliveira1234@gmail.com>
2019-05-01 13:59:27 -0300

    Funcionamento base do formulario implementado

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 var xml;
 var opt = document.getElementById('options');
 var quizQst;
+var globalIndex;
 var qstList = [];
 var dicionario = [];
 
 $(document).ready(function(){
-    document.getElementById('pag1').style.backgroundColor = 'black';
+    // document.getElementById('pag1').style.backgroundColor = 'black';
 
     quizQst = document.getElementById('quizQuestion');
     let ajax = new XMLHttpRequest();
@@ -38,9 +39,19 @@ $(document).ready(function(){
     ajax.send();
 })
 
-function fulfillPages(index, a){
-    console.log(document.getElementById('options'));
-    console.log(a);
+function fulfillPages(index){
+    globalIndex = index;
+
+    resetColors();
+
+    document.getElementById('pag' + index).style.backgroundColor = '#4CAF50'
+    document.getElementById('pag' + index).style.color = 'white'
+
+    for(let i = 0; i < dicionario.length; i++){
+        if(dicionario[i].Aba == index){
+            document.getElementById(dicionario[i].Resp).style.backgroundColor = 'red';
+        }
+    }
 
     quizQst.innerHTML = qstList[index].Titulo;
     document.getElementById('questao1').innerHTML = qstList[index].Questao1;
@@ -51,13 +62,63 @@ function fulfillPages(index, a){
 
 function selectThis(item){
     let element = document.getElementById(item.id);
-    console.log(element.style.backgroundColor);
+    let deleted = false;
+
+    if(!deleted){
+        obj = {
+            Aba: globalIndex,
+            Resp: element.id
+        }
     
+        updateOrInsert(obj);
+    }
+    console.log(dicionario)
+
     if(element.style.backgroundColor == 'red'){
         element.style.backgroundColor = '#4CAF50';
+        deleted = true;
+        removeFromDictionary(element.id);
     }
     else
+    {
+        resetColors();
         element.style.backgroundColor = 'red';
+    }
+}
+
+function updateOrInsert(obj){
+
+    let boolKey = 0;
+
+    for(let i = 0; i < dicionario.length; i++){
+        if(dicionario[i].Aba == obj.Aba){ //UPDATE
+            dicionario[i].Resp = obj.Resp;
+            boolKey = 1;
+        }
+    }
+
+    if(boolKey == 0){ //INSERT
+        dicionario.push(obj);
+    }
+}
+
+function removeFromDictionary(item){
+    dicionario[globalIndex].Resp = "";
+}
+
+function resetColors(){
+    document.getElementById('questao1').style.backgroundColor = "#4CAF50"
+    document.getElementById('questao2').style.backgroundColor = "#4CAF50"
+    document.getElementById('questao3').style.backgroundColor = "#4CAF50"
+    document.getElementById('questao4').style.backgroundColor = "#4CAF50"
+
+    for(let i = 0; i < 10; i++){
+        document.getElementById('pag' + i).style.backgroundColor = 'white' 
+        document.getElementById('pag' + i).style.color = 'black' 
+    }
+}
+    // let teste = document.getElementById('pag1');
+    // console.log(dicionario);
+    
     //o segredo é pegar o id do item clicado, o id da pagina atual
     //e linka-los atraves de um JSON chave/valor
-}
